e5a1bc9544a7add621975d8cbe2d2982f4166904
Joshua Ackerman <joshackerman757@gmail.com>
2023-12-07 21:45:58 -0500

    done

--- api/server.test.js
+++ api/server.test.js
@@ -21,17 +21,17 @@ test('[1] sanity', () => {
 
 describe('[2] API Endpoints', () => {
   describe('[3] [POST] /api/auth/register', () => {
-    // it('[4] should register a new user', async () => {
-    //   const newUser = { username: 'testuser', password: 'testpassword' };
-    //   const res = await request(server)
-    //     .post('/api/auth/register')
-    //     .send(newUser);
+    it('[4] should register a new user', async () => {
+      const newUser = { username: 'testuser', password: 'testpassword' };
+      const res = await request(server)
+        .post('/api/auth/register')
+        .send(newUser);
 
-    //   expect(res.status).toBe(201);
-    //   expect(res.body).toHaveProperty('id');
-    //   expect(res.body).toHaveProperty('username', 'testuser');
-    //   expect(res.body).toHaveProperty('password');
-    // });
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty('id');
+      expect(res.body).toHaveProperty('username', 'testuser');
+      expect(res.body).toHaveProperty('password');
+    });
 
     it('[5] should return 400 on missing username or password', async () => {
       const res = await request(server)
@@ -58,20 +58,20 @@ describe('[2] API Endpoints', () => {
   });
 
   describe('[7] [POST] /api/auth/login', () => {
-    // it('[8] should login an existing user', async () => {
-    //   const existingUser = { username: 'existinguser', password: 'existingpassword' };
-    //   await request(server)
-    //     .post('/api/auth/register')
-    //     .send(existingUser);
+    it('[8] should login an existing user', async () => {
+      const existingUser = { username: 'existinguser', password: 'existingpassword' };
+      await request(server)
+        .post('/api/auth/register')
+        .send(existingUser);
 
-    //   const res = await request(server)
-    //     .post('/api/auth/login')
-    //     .send(existingUser);
+      const res = await request(server)
+        .post('/api/auth/login')
+        .send(existingUser);
 
-    //   expect(res.status).toBe(200);
-    //   expect(res.body).toHaveProperty('message', 'welcome, existinguser');
-    //   expect(res.body).toHaveProperty('token');
-    // });
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('message', 'welcome, existinguser');
+      expect(res.body).toHaveProperty('token');
+    });
 
     it('[9] should return 400 on missing username or password', async () => {
       const res = await request(server)
@@ -93,25 +93,25 @@ describe('[2] API Endpoints', () => {
     });
   });
 
-  // it('[11] should return jokes with a valid token', async () => {
-  //   const newUser = { username: 'jokeuser', password: 'jokepassword' };
-  //   await request(server)
-  //     .post('/api/auth/register')
-  //     .send(newUser);
+  it('[11] should return jokes with a valid token', async () => {
+    const newUser = { username: 'jokeuser', password: 'jokepassword' };
+    await request(server)
+      .post('/api/auth/register')
+      .send(newUser);
 
-  //   const loginRes = await request(server)
-  //     .post('/api/auth/login')
-  //     .send(newUser);
+    const loginRes = await request(server)
+      .post('/api/auth/login')
+      .send(newUser);
 
-  //   const token = loginRes.body.token;
+    const token = loginRes.body.token;
 
-  //   const res = await request(server)
-  //     .get('/api/jokes')
-  //     .set('Authorization', token);
+    const res = await request(server)
+      .get('/api/jokes')
+      .set('Authorization', token);
 
-  //   expect(res.status).toBe(200);
-  //   expect(res.body).toHaveLength(3);
-  // });
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(3);
+  });
 
   it('[12] should return 401 on missing or invalid token', async () => {
     const resWithoutToken = await request(server).get('/api/jokes');
